953e512cc26641e6b089f9e85e33b96ea4fee7eb
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-01-03 15:55:45 +0000

    Fixed allowed host

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -35,7 +35,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
+    ALLOWED_HOSTS = [os.environ.get('introduction-django.herokuapp.com')]
 
 
 # Application definition
