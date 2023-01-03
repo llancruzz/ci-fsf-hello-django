9dabe546e205419a64c1f40457b47724070fd91e
Alan Cruz da Silva <alancruzsilva@gmail.com>
2023-01-03 16:05:12 +0000

    Fixed allowed host in settigns

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = development
 
 if development:
-    ALLOWED_HOSTS = ['localhost']
+    ALLOWED_HOSTS = ['introduction-django.herokuapp.com']
 else:
     ALLOWED_HOSTS = [os.environ.get('introduction-django.herokuapp.com')]
 
